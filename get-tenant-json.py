08c2490fe9654efab9b284e63e53df847cf9b856
Ali Shalhoob <alishalhoob@hotmail.com>
2021-01-16 17:42:48 -0800

    adding xmlbypython.py

--- get-tenant-json.py
+++ get-tenant-json.py
@@ -15,4 +15,15 @@ encoded_body = json.dumps({
 resp = requests.post("https://sandboxapicdc.cisco.com/api/aaaLogin.json", data=encoded_body, verify=False)
 header = {"Cookie": "APIC-cookie=" + resp.cookies["APIC-cookie"]}
 tenants = requests.get("https://sandboxapicdc.cisco.com/api/node/class/fvTenant.json?rsp-subtree-include=health,faults", headers=header, verify=False)
-print(tenants.text)
+### print JSON as text###
+#print(tenants.text)
+### print JASON as JSON object###
+json_response = json.loads(tenants.text)
+#print(json.dumps(json_response, sort_keys=True, indent=4))
+json_tenants = json_response['imdata']
+for tenant in json_tenants:
+ tenant_name = tenant['fvTenant']['attributes']['name']
+ tenant_dn = tenant['fvTenant']['attributes']['dn']
+ tenant_health = tenant['fvTenant']['children'][0]['healthInst']['attributes']['cur']
+ output = "Tenant: " + tenant_name + "\t Health Score: " + tenant_health + "\n DN: " + tenant_dn
+ print(output.expandtabs(40))
